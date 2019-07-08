364f109c51d60b47a8e9d7ee65cdfebd0ad4b6df
skyler dowdy <skylerwebdev@gmail.com>
2019-07-08 19:05:50 -0400

    work on digital timer

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,44 @@
+const msTens = document.getElementById("msTens");
+const msHundreds = document.getElementById("msHundreds");
+const secondOnes = document.getElementById("secondOnes");
+const secondTens = document.getElementById("secondTens");
+msTens.innerHTML = 0;
+for (let i = 0; i <10; i++) {
+setTimeout(() => {
+  msTens.innerHTML=[i]
+}, [i]); continue}
+// let i = 0;
+
+  // setTimeout(() => {
+//   msTens.innerHTML='1'
+// }, 1);
+// setTimeout(() => {
+//   msTens.innerHTML='2'
+// }, 2);
+// setTimeout(() => {
+//   msTens.innerHTML='3'
+// }, 3);
+// setTimeout(() => {
+//   msTens.innerHTML='4'
+// }, 4);
+// setTimeout(() => {
+//   msTens.innerHTML='5'
+// }, 5);
+// setTimeout(() => {
+//   msTens.innerHTML='6'
+// }, 6);
+// setTimeout(() => {
+//   msTens.innerHTML='7'
+// }, 7);
+// setTimeout(() => {
+//   msTens.innerHTML='8'
+// }, 8);
+// setTimeout(() => {
+//   msTens.innerHTML='9'
+// }, 9);
+// setTimeout(() => {
+//   msHundreds.innerHTML='1'
+// }, 10);
+// setTimeout(() => {
+//   msTens.innerHTML='0'
+// }, 10);
